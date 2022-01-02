cf68aa7d3a62f9efd9c4c5cf154af53546f26752
Rajudar <93924876+Rajudar@users.noreply.github.com>
2022-01-02 12:21:57 +0530

    Create assignment1 all even numbers below n.js

---
+++ DEC23/assignment1 all even numbers below n.js
@@ -0,0 +1,12 @@
+//all even numbers bellow given value n .
+
+var n = prompt("enter any value");
+console.log("even number is between:" + n);
+
+for(var a=0;a<=n;a++) 
+  {
+  if(a%2==0) 
+   {
+    console.log(a);
+   }
+}
