bff0cc8936764d8d3a20389fcde0ee00e0d4d0bf
Rajudar <93924876+Rajudar@users.noreply.github.com>
2022-01-02 12:07:15 +0530

    Create assignment2 vowel or not.js

---
+++ DEC21/assignment2 vowel or not.js
@@ -0,0 +1,26 @@
+var char = prompt("Enter any character");
+
+if(char == "a") {
+  alert("it is a vowel");
+}  else if(char == "A") {
+  alert("it is a vowel");
+}  else if(char == "e") {
+  alert("it is a vowel");
+}  else if(char == "E") {
+  alert("it is a vowel");
+}  else if(char == "i") {
+  alert("it is a vowel");
+}  else if(char == "I") {
+  alert("it is a vowel");
+}  else if(char == "o") {d
+
+  alert("it is a vowel");
+}  else if(char == "O") {
+  alert("it is a vowel");
+}  else if(char == "u") {
+  alert("it is a vowel");
+}  else if(char == "U") {
+  alert("it is a vowel");
+}  else{
+  alert("it is a consonant");
+}
