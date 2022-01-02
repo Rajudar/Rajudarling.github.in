ad2d01d7210070a99865007a3c3a6fac9fb82844
Rajudar <93924876+Rajudar@users.noreply.github.com>
2022-01-02 12:04:15 +0530

    Create minimum value using prompt.js

---
+++ DEC21/minimum value using prompt.js
@@ -0,0 +1,11 @@
+var a = prompt("enter the num3");
+var b = prompt("enter the num4");
+var c = prompt("enter the num5");
+
+if(a<b && a<c) {
+  alert("a is small");
+} else if(b<a && b<c) {
+  alert("b is small");
+} else {
+  alert("c is small");
+}
