be2b7ddf2690a5697b905d6d8c64658aefc32ecf
Rajudar <93924876+Rajudar@users.noreply.github.com>
2022-01-02 12:34:37 +0530

    Create assignment2.first n prime numbers.js

---
+++ DEC24/assignment2.first n prime numbers.js
@@ -0,0 +1,17 @@
+//take n input from user and to print first n prime numbers.
+
+var enterNumber = prompt("Enter number: ");
+
+for(var i=2; i<=enterNumber ;i++){
+
+        var isPrime = true;
+
+        for(var j=2; j<i; j++){
+            if(i%j === 0 && i !== j){
+                isPrime = false;
+            }
+        }
+        if(isPrime === true){
+            console.log(i);
+        }
+    } 
